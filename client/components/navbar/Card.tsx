464eb1c3b89657908461fdf716855a52b542368c
Thierry Davelaar <thierrydavelaar@Thierrys-MacBook-Pro.local>
2022-07-12 15:03:16 +0200

    angled shadow cards placed

--- client/components/navbar/Card.tsx
+++ client/components/navbar/Card.tsx
@@ -11,8 +11,9 @@ type Props = {
 };
 
 //######################### COMPONENT STYLES ###############################################
-const Wrapper = tw.div`grid auto-rows-min py-3 px-7 gap-3 border-gray-500 border-l-[0.1px] border-r-[0.1px] `;
+const Wrapper = tw.div`relative grid auto-rows-min py-3 px-10 gap-3 border-gray-500`;
 
+const Shadow = tw.div`absolute w-full h-full skew-x-[-12deg] border-opacity-20 border-l-[1px] border-r-[1px]`;
 const Row = tw.div`grid grid-flow-col gap-4 min-w-[15rem]`;
 
 const MatchWrapper = tw(Row)``;
@@ -24,27 +25,16 @@ const OddsWrapper = tw(Row)``;
 const Card: React.FC<Props> = ({ home, draw, away }) => {
   return (
     <Wrapper>
+      <Shadow />
       <MatchWrapper>
         <Team name="FEY" imgurl="./tshirt-icon.png" home />
         <MatchInfo date="WO 15/5" time="21:00" />
         <Team name="FEY" imgurl="./tshirt-icon.png" home={false} />
       </MatchWrapper>
       <OddsWrapper>
-        <Odd
-          label={home.label}
-          odd={home.score}
-          url={home.url}
-        />
-        <Odd
-          label={draw.label}
-          odd={draw.score}
-          url={draw.url}
-        />
-        <Odd
-          label={away.label}
-          odd={away.score}
-          url={away.url}
-        />
+        <Odd label={home.label} odd={home.score} url={home.url} />
+        <Odd label={draw.label} odd={draw.score} url={draw.url} />
+        <Odd label={away.label} odd={away.score} url={away.url} />
       </OddsWrapper>
     </Wrapper>
   );
